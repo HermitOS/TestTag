c19c25d7a65268d567c03a6c7df297e91eb4622a
Terje Sandstrom <terje@hermit.no>
2025-12-08 12:26:06 +0100

    Added code and build files

---
+++ TagTest.ConsoleApp.Tests/TagTest.Tests/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using TagTest.ConsoleApp;
+
+namespace TagTest.Tests;
+
+public class CalculatorTests
+{
+    private Calculator _calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+    {
+        // Arrange
+        int a = 5;
+        int b = 3;
+
+        // Act
+        int result = _calculator.Add(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(8));
+    }
+
+    [Test]
+    public void Add_NegativeAndPositiveNumber_ReturnsCorrectSum()
+    {
+        // Arrange
+        int a = -5;
+        int b = 3;
+
+        // Act
+        int result = _calculator.Add(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-2));
+    }
+
+    [Test]
+    public void Add_TwoZeros_ReturnsZero()
+    {
+        // Arrange & Act
+        int result = _calculator.Add(0, 0);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(0));
+    }
+}
---
+++ TagTest.ConsoleApp/TagTest.ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using TagTest.ConsoleApp;
+
+var calculator = new Calculator();
+var result = calculator.Add(5, 3);
+Console.WriteLine($"5 + 3 = {result}");
